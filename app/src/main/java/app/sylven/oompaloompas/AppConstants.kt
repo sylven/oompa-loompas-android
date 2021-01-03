05149be1bbb53dd61394a57d7220f4ccdeba1376
Silviu Chirvasa <silviuchirvasa@gmail.com>
2021-01-03 19:09:42 +0100

    Add api factory

---
+++ app/src/main/java/app/sylven/oompaloompas/AppConstants.kt
@@ -0,0 +1,7 @@
+package app.sylven.oompaloompas
+
+object AppConstants {
+
+    const val OOMPA_LOOMPA_API_BASE_URL = "https://2q2woep105.execute-api.eu-west-1.amazonaws.com/napptilus/"
+
+}
---
+++ app/src/main/java/app/sylven/oompaloompas/networking/ApiFactory.kt
@@ -0,0 +1,37 @@
+package app.sylven.oompaloompas.networking
+
+import app.sylven.oompaloompas.AppConstants
+import app.sylven.oompaloompas.BuildConfig
+import app.sylven.oompaloompas.api.OompaApi
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object ApiFactory {
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val oompaClient =
+        if (BuildConfig.DEBUG) {
+            OkHttpClient().newBuilder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+        } else {
+            OkHttpClient().newBuilder()
+                .build()
+        }
+
+    fun retrofit(): Retrofit = Retrofit.Builder()
+        .client(oompaClient)
+        .baseUrl(AppConstants.OOMPA_LOOMPA_API_BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .build()
+
+    val oompaApi: OompaApi = retrofit().create(OompaApi::class.java)
+
+}
