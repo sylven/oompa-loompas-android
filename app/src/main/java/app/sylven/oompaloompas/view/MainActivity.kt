1e8cf473ec06a9983ece2d3a19b6171931aa64a1
Silviu Chirvasa <silviuchirvasa@gmail.com>
2021-01-03 19:15:18 +0100

    Add MainActivity

---
+++ app/src/main/java/app/sylven/oompaloompas/view/MainActivity.kt
@@ -0,0 +1,29 @@
+package app.sylven.oompaloompas.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import app.sylven.oompaloompas.R
+import app.sylven.oompaloompas.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+
+}
