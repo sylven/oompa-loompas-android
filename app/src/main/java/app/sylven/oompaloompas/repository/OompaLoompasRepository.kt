355cf8955db960e03bd9d741bf5b8c01f7a89bdf
Silviu Chirvasa <silviuchirvasa@gmail.com>
2021-01-03 19:13:14 +0100

    Add Oompas repository

---
+++ app/src/main/java/app/sylven/oompaloompas/repository/OompaLoompasRepository.kt
@@ -0,0 +1,42 @@
+package app.sylven.oompaloompas.repository
+
+import android.content.Context
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import app.sylven.oompaloompas.model.OompaLoompa
+import app.sylven.oompaloompas.model.OompaLoompaPageItem
+import app.sylven.oompaloompas.networking.ApiFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+class OompaLoompasRepository(context: Context) {
+    
+    private val oompaService = ApiFactory.oompaApi
+
+    @OptIn(ExperimentalPagingApi::class)
+    fun fetchOompaLoompas(): Flow<PagingData<OompaLoompaPageItem>> {
+        return Pager(
+            PagingConfig(
+                pageSize = 25,
+                enablePlaceholders = false,
+                initialLoadSize = 25,
+                prefetchDistance = 18
+            )
+        ) {
+            OompaLoompasPagingSource(
+                oompaService = oompaService
+            )
+        }.flow.flowOn(Dispatchers.IO)
+    }
+
+    fun fetchOompaLoompa(id: Int): Flow<OompaLoompa> {
+        return flow {
+            oompaService.getOompaLoompa(id).body()?.let { emit(it) }
+        }.flowOn(Dispatchers.IO)
+    }
+
+}
